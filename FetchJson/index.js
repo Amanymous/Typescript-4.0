d90742d7e00884d75fa53d51e8fbeff25338843e
aman mirza <aman mirza99>
2020-09-03 01:50:19 +0500

    second commit

--- FetchJson/index.js
+++ FetchJson/index.js
@@ -1,7 +1,34 @@
 "use strict";
 exports.__esModule = true;
 var axios_1 = require("axios");
-var url = 'http://jsonplaceholder.typicode.com/todos/1';
+var url = 'http://jsonplaceholder.typicode.com/todos/2';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("\n  todo with id:" + id + "\n  todo with title:" + title + "\n  todo with completed:" + completed + "\n  ");
 });
+// import axios from 'axios';
+// const url = 'http://jsonplaceholder.typicode.com/todos/1';
+// interface Todo {
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
+// axios.get(url).then((response) => {
+//   const todo = response.data;
+//   const id = todo.id;
+//   const title = todo.title;
+//   const completed = todo.completed;
+// logTodo(id, title, completed);
+// if i am changing the sequence of completed,title,id then i have define the function
+// like id: number, title: string, completed: boolean this wrong u can't change sequence at all
+// });
+// const logTodo = (id: number, title: string, completed: boolean) => {
+//   console.log(`
+//     Todo with ID: ${id}
+//     Todo with title: ${title}
+//     Todo with finished: ${completed}
+//   `);
+// };
--- FetchJson/index.ts
+++ FetchJson/index.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const url = 'http://jsonplaceholder.typicode.com/todos/2';
+// const url = 'http://jsonplaceholder.typicode.com/todos/2';
 
 // axios.get(url).then((response) => {
 //   const todo = response.data;
@@ -8,22 +8,38 @@ const url = 'http://jsonplaceholder.typicode.com/todos/2';
 //   const title = todo.title;
 //   const completed = todo.completed;
 
-//   console.log(response.data);
 //   console.log(`
-//   Todo with ID:${id}
-//   tODO WITH TITLE:${title}
-//   TODO WITH FINISHED:${completed}
+//   todo with id:${id}
+//   todo with title:${title}
+//   todo with completed:${completed}
 //   `);
 // });
+
+import axios from 'axios';
+
+const url = 'http://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  const todo = response.data;
+  const todo = response.data as Todo;
   const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
 
-  console.log(`
-  todo with id:${id}
-  todo with title:${title}
-  todo with completed:${completed}
-  `);
+  logTodo(id, title, completed);
+  // if i am changing the sequence of completed,title,id then i have define the function
+  // like id: number, title: string, completed: boolean this wrong u can't change sequence at all
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    Todo with ID: ${id}
+    Todo with title: ${title}
+    Todo with finished: ${completed}
+  `);
+};
