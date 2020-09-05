c53218eea0f5f513ff921d195fa1b6b36ee15703
aman mirza <aman mirza99>
2020-09-06 00:47:01 +0500

    task 6 branch class method modifier and fields classes

--- features/classes.ts
+++ features/classes.ts
@@ -10,22 +10,68 @@
 // const vehicle = new Vehicle();
 // vehicle.drive();
 // vehicle.honk();
+// ----------------------------------------
 
+// class Vehicle {
+// protected drive(): void {
+//   console.log('yoho');
+// }
+//   color: string = 'red';
+
+//   protected honk(): void {
+//     console.log('honk');
+//   }
+// }
+
+// const vehicle = new Vehicle();
+// console.log(vehicle.color);
+
+// class Cars extends Vehicle {
+//   private drive(): void {
+//     console.log('cars');
+//   }
+
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
+
+// const carss = new Cars();
+// carss.startDrivingProcess();
+// // carss.honk();
+// ---------------------------------
+
+// class Vehicle {
+//   color: string;
+//   protected drive(): void {
+//     console.log('yoho');
+//   }
+//   constructor(color: string) {
+//     this.color = color;
+//   }
+
+//   protected honk(): void {
+//     console.log('honk');
+//   }
+// }
+
+// const vehicle = new Vehicle('orange');
+// console.log(vehicle.color);
+
+// -----------------------------------
 class Vehicle {
-  drive(): void {
+  protected drive(): void {
     console.log('yoho');
   }
-  honk(): void {
+  constructor(public color: string) {
+    this.color = color;
+  }
+  // only public able to run on it not private and protected
+  protected honk(): void {
     console.log('honk');
   }
 }
 
-class Cars extends Vehicle {
-  drive(): void {
-    console.log('cars');
-  }
-}
-
-const carss = new Cars();
-carss.drive();
-carss.honk();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
