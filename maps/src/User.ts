842638dc7ae5c5d1bf965f30e0c2c27f600f7f06
aman mirza <aman mirza99>
2020-09-14 23:14:06 +0500

    maps app is completed

---
+++ maps/src/User.ts
@@ -0,0 +1,23 @@
+// lat means latitude and lang means longitude
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'red';
+  constructor() {
+    this.name = faker.name.firstName();
+    console.log(this.location);
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+  markerContent(): string {
+    return `User Name:${this.name}`;
+  }
+}
